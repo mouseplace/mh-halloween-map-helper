942feddbde6d66c8ad927e4a3e24f883cc38c658
Brad Parbs <brad@bradparbs.com>
2022-10-13 12:59:24 -0500

    oops tags are hard

--- mh-halloween-map-helper.user.js
+++ mh-halloween-map-helper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         🐭️ MouseHunt - Halloween Map Helper
-// @version      1.0.1
+// @version      1.1.2
 // @description  Easily see and equip the cheese needed for Halloween Trick or Treat maps.
 // @license      MIT
 // @author       bradp
